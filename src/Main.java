582b0c6190a85d1553a704f0ccec90d2f5d676e7
malkov <malkov922522@gmail.com>
2025-05-10 12:36:39 +0400

    commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 14375;
+        int bonusOneMiles = 20;
+        int bonus = price / bonusOneMiles;
+
+        System.out.println("Начислено миль: " + bonus);
+
+     }
+    }
